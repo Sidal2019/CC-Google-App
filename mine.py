84f65e46938d2ec5712ec6eabf5e9bb294e68cc0
Sidal2019 <99678310+Sidal2019@users.noreply.github.com>
2023-05-23 08:33:23 +0530

    Create mine.py

---
+++ mine.py
@@ -0,0 +1,24 @@
+Console
+Select Project
+NEW PROJECT
+Enter Project Name
+Select Project
+Search App Engine
+Create New Applications
+Next
+i'll do this later
+Get Started
+App Engine Admine API
+Enable
+Cloud Shell icon in right >_
+Open Git hub 
+createrepository
+create file goto code
+copy the url
+come to cloud 
+git clone + url
+cd repository name
+ls 
+python + file name
+done
+
